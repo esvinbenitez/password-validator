e91677d058e379d41f1b9e9bdfc7dbc77eb8c453
esvin benitez <esvin@Esvins-MBP.hsd1.ma.comcast.net>
2023-04-17 18:14:35 -0400

    password-V

--- src/script.js
+++ src/script.js
@@ -3,8 +3,24 @@ function  checkCorrectPassword() {
         //get the password    
         //had each condition be false
     let password = document.getElementById("password") 
-   
+
+    let checkCorrectPassword = Array.from("password")
+    PasswordLength(password)
+}
+
+    function PasswordLength (passwordArray){
+        let minChar = 8
+        if (minChar <= PasswordLength.length){
+            character(passwordArray)
+        } 
+        else {
+            alert("This is not valid password")
+        }
+
+    }
+
     //had each condition be false
+    function character(passwordArray){
     let LowerCase = false
     let UpperCase = false
     let Number = false
@@ -12,24 +28,19 @@ function  checkCorrectPassword() {
     let SpecialChar = ["!","@","#","$","%","^","&","*","(",")","_","-","+","|","[","]","{","}",":",";","/","?",".",","]
     //looked at each character in the password
     //checked if the character was lower case
-            //when it met the criteria we marked that condition as true
-        for(let i = 0; i < checkCorrectPassword.length ; i++){
-            let = password [i]
-        }
-         if(password[i] == LowerCase(password[i])){
-          LowerCase = true;
-        }
+            //when it met the criteria we marked that condition as tr
 
-           else if(password[i] == UpperCase(password[i])){ 
-           UpperCase = true;
-        } 
-          
-           else if(password[i] == Number(password[i])){
-           Number = true;
-        } 
-       
-        else if(SpecialChar.indexof(password[i]) >=0){ 
-            isSpecialChar = true;
-        }
+        for(let i = 0; i < password.length ; i++){
+            
+        
+        if (char (/[a-z]/g))   {LowerCase = true}
 
-    }
+        else if (char(/(A-Z)/g))  {UpperCase = true}
+
+        else if (char(/(0-9)/g))  {Number = true}
+
+        else if (char(/(!,@,$,%,^,&,*)/g)) {SpecialChar = true}
+
+        }
+        
+        }
